9515a355d924077cb01d0e2e4a2b7140a8caba0b
Max Beier <beierm@htw-berlin.de>
2018-07-08 21:14:40 +0200

    condensed `XmlAsset`

--- XmlAsset.js
+++ XmlAsset.js
@@ -1,20 +1,19 @@
-const { Asset } = require('parcel-bundler');
+// const { Asset } = require('parcel-bundler');
+const Asset = require('../parcel-bundler/src/Asset.js');
 
 class XmlAsset extends Asset {
+
   constructor(name, pkg, options) {
     super(name, pkg, options);
-    this.type = 'xml';
-  }
-
-  parse(source) {
-    this.xml = source;
+    this.type = 'js';
   }
 
   generate() {
     return {
-      js: `module.exports = ${JSON.stringify(this.xml)}`,
+      js: `module.exports = ${JSON.stringify(this.contents)}`,
     };
   }
+
 }
 
 module.exports = XmlAsset;
